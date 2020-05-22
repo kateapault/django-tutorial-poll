dd6feb6bcecc6a6f1d839835fce9c5831e558470
github username <kateapault@gmail.com>
2020-05-22 17:20:27 -0400

    first half of part 5: aded tests for was_published_recently method for questions

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is in the future.
+        """
+        future_time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_time)
+        self.assertIs(future_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is more than 1 day ago.
+        """
+        one_day_ago = timezone.now() - datetime.timedelta(hours=24, seconds=1)
+        old_question = Question(pub_date=one_day_ago)
+        self.assertIs(old_question.was_published_recently(), False)
+        
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date is between now and 24h ago
+        """
+        within_24h = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=within_24h)
+        self.assertIs(recent_question.was_published_recently(), True)
+        
+    
